d09a6ee29fd80abd36fe3d945581d2ca0be24ac6
Ananya Heroor <47873321+AnanyaHeroor@users.noreply.github.com>
2020-08-08 14:25:38 -0700

    Update Combinations.java
    Combinations.findColorCombination: 2.42/3.00 --- Input: 2 3 4 true true Output: + (Your output: N) --- Input: 6 12 2 true true Output: + (Your output: N) --- Input: 6 3 4 true true Output: + (Your output: N) --- Input: 2 3 12 true true Output: * (Your output: N) --- Input: 3 54 4 true true Output: * (Your output: N) --- Input: -8 -1 4 true true Output: * (Your output: N)

--- Combinations.java
+++ Combinations.java
@@ -1,4 +1,7 @@
-//Ananya Heroor, 9074986762, heroor
+/**
+* This class combines numbers to create a game at the library
+* @author Ananya Heroor
+*/
 public class Combinations{
     public static void main (String [] args){
         System.out.println(findCombination(10,10,100));
@@ -14,6 +17,14 @@ public class Combinations{
         System.out.println(findColorCombination(5, 2, 10, true, true));
         System.out.println(findColorCombination(5, 2, 9, false, true));
     }
+ /**
+ * This function determines whether any two numbers can be added or multiplied to produce the third (order doesn't matter
+ * @param int x
+ * @param int y
+ * @param int z
+ * @return A char: + if two of the inputs can be added to make a third, * if
+    multiplied, and N if there is no combination
+ */
     public static char findCombination (int x, int y, int z){
        if ((x * y == z) || (x * z ==  y)  ||  (y *  z ==  x)){
            return '*';
@@ -23,6 +34,15 @@ public class Combinations{
            return 'N';
        }
     }
+/**
+ * This function determines whether any two numbers can be subtracted or divided to one another (order doesn't matter
+ * @param int x
+ * @param int y
+ * @param int z
+ * @return a char: - if two of the inputs can be subtracted to make the third,
+    / if two of the inputs can be divided to make the thirds, and N if
+    there is no combination
+ */
     public static char findOppositeCombination (int x, int y, int z){
         char ret = findCombination(x,y,z);
         if (ret == '*'){
@@ -33,6 +53,19 @@ public class Combinations{
             return 'N';
         }
     }
+/**
+* This functon determines whether three integers add or multiply to one another AND there will be a ball
+    which will either be red, blue or both.  If the ball is red, the first and
+    third integers are multiplied by 3. If the ball is blue, then the second integer is multiplied by 2. If
+    the ball is both red and blue, then both happen.
+* @param int x
+* @param int y
+* @param int z
+* @param boolean isRed
+* @param boolean isBlue
+* @return A char that is either +, *, or N, according to the rules in the
+    paragraph above...
+*/
     public static char findColorCombination (int x, int y, int z, boolean isRed, boolean isBlue){
         if (isRed){
             x = x* 3;
