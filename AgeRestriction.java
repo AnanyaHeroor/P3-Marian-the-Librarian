ad409543d74d0b259de16d5a3bf2c19e33fe3650
Ananya Heroor <47873321+AnanyaHeroor@users.noreply.github.com>
2020-08-08 13:56:33 -0700

    Update AgeRestriction.java
    Marian’s library is facing a major epidemic. Some of the local schoolchildren have been checking
    out adult books that are entirely inappropriate for their age. These books contain topics and
    illustrations that might scare children. In light of this, Marian needs you to write a program to help figure out which books a given person is allowed to borrow.

--- AgeRestriction.java
+++ AgeRestriction.java
@@ -1,4 +1,7 @@
-//Ananya Heroor, 9074986762, heroor
+/**
+* This class figures out which books a given person is allowed to borrow
+* @author Ananya Heroor
+*/
 public class AgeRestriction{
     public static void main (String[] args){
         System.out.println(isOver13(5));
@@ -9,6 +12,12 @@ public class AgeRestriction{
         whatCanIBorrow(-15);
         whatCanIBorrow(150);
     }
+/**
+* This function checks if someone is under the age of 13
+* kids under the age of 13 should not be allowed to borrow books
+* @param int age: the borrower's age
+* @return boolean: true if age is 13 or above, false otherwise
+*/
     public static boolean isOver13(int age){
         if (age >= 13){
             return true;
@@ -16,11 +25,18 @@ public class AgeRestriction{
             return false;
         }
     }
+/**
+* This function checks if ages fall in the range of 13 to under 18
+* If the borrower is 13 or older, but still under 18, they will not be allowed to borrow any books
+    containing sensitive materials, but will still be allowed to borrow anything else
+* @param int age: the borrower's age
+* @return nothing
+*/
     public static boolean whatCanIBorrow(int age){
         if (age >=18 && age < 100){
             System.out.println ("You can borrow any book!!!");
         } else if (age < 18 && age > 13){
-            System.out.println("You cannot borrow any sensitive material.");
+            System.out.println("You cannot borrow sensitive material.");
         } else if (age < 13 && age > 0){
             System.out.println("You cannot borrow any books.");
         } else if (age < 0){
