1a6d93ba959dd55c18481af251932e9f14eda5fd
Ananya Heroor <47873321+AnanyaHeroor@users.noreply.github.com>
2020-08-08 14:16:38 -0700

    Update LateFees.java
    LateFees.isAccountBad: 1.50/2.00 --- Input: false false Output: false (Your output: true) ************************************ LateFees.isAccountGood: 1.00/1.00

--- LateFees.java
+++ LateFees.java
@@ -1,4 +1,7 @@
-//Ananya Heroor, 9074986762, heroor
+/**
+* This class determines which accounts need to pay fines
+* @author Ananya Heroor
+*/
 public class LateFees{
     public static void main (String [] args){
        System.out.println(isAccountBad(true,true));
@@ -7,6 +10,16 @@ public class LateFees{
        System.out.println(isAccountGood(false, true));
 
     }
+/**
+* This function determines which accounts are bad and need to pay fines
+* An account is considered bad if either (a) the account paid money, but has no overdue books, or
+    (b) the account has not paid, but does have an overdue book.
+* @param boolean paidLateFee: has this account paid a late fee?
+* @param boolean hasOverdueBook: does this account have an overdue
+    book?
+* @return boolean that is true if the account is (a) paid and not overdue, or
+    (b) overdue and not paid.
+*/
     public static boolean isAccountBad (boolean paidLateFee, boolean hasOverdueBook){
         if (paidLateFee && hasOverdueBook){
             return false;
@@ -14,6 +27,14 @@ public class LateFees{
             return true;
         }
     }
+/**
+* This function determines which accounts are good and don't need to pay fines
+* @param boolean paidLateFee: has this account paid a late fee?
+* @param boolean hasOverdueBook: does this account have an overdue
+    book?
+* @return boolean that is true if the account is (a) paid and overdue, or
+    (b) overdue and not paid.
+*/
     public static boolean isAccountGood (boolean paidLateFee, boolean hasOverdueBook){
         if ((paidLateFee && hasOverdueBook) || (!paidLateFee && !hasOverdueBook)){
             return true;
