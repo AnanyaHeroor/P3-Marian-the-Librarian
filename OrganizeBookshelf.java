a497ed768e425126dd78f6d8e5958e49e881cede
Ananya Heroor <47873321+AnanyaHeroor@users.noreply.github.com>
2020-08-08 14:10:09 -0700

    Update OrganizeBookshelf.java
    OrganizeBookshelf.getRightmostBookID: 0.00/1.00 --- GetRightmostBookIDTest.java:16: error: incompatible types: inference variable T has incompatible bounds --- return Optional.of(OrganizeBookshelf.getRightmostBookID(parameters.id_1(), parameters.id_2())); --- ^ --- equality constraints: Integer --- lower bounds: Boolean --- where T is a type-variable: --- T extends Object declared in method <T>of(T) --- 1 error
    
    OrganizeBookshelf.spaceToLeave: 0.00/2.00 --- SpaceToLeaveTest.java:16: error: incompatible types: inference variable T has incompatible bounds --- return Optional.of(OrganizeBookshelf.spaceToLeave(parameters.id_1(), parameters.id_2())); --- ^ --- equality constraints: Integer --- lower bounds: Boolean --- where T is a type-variable: --- T extends Object declared in method <T>of(T) --- 1 error

--- OrganizeBookshelf.java
+++ OrganizeBookshelf.java
@@ -1,4 +1,7 @@
-//Ananya Heroor, 9074986762, heroor
+/**
+* This class puts books back in the correct order
+* @author Ananya Heroor
+*/
 public class OrganizeBookshelf{
     public static void main (String [] args){
         getRightmostBookID(5, 10);
@@ -10,6 +13,12 @@ public class OrganizeBookshelf{
         spaceToLeave(-5,10);
         spaceToLeave(5,-10);
     }
+/**
+* This function sorts shelves so that the IDs increase from left the right
+* @param int id_1: book 1’s ID number
+* @param int id_2: book 2’s ID number
+* @return int: ID of the book that should be placed on the right
+*/
     public static boolean getRightmostBookID (int id_1, int id_2){
         if (id_1 > id_2){
             System.out.println(id_1);  
@@ -19,6 +28,14 @@ public class OrganizeBookshelf{
         return true;
         
     }
+/**
+* This function that takes two integers, and returns how much space Marian
+    needs to leave between them on the shelves
+* @param int id_1: book 1’s ID number
+* @param int id_2 book 2’s ID number
+* @return int that represents the number of integers/spaces between id_1
+    and id_2
+*/
     public static boolean spaceToLeave (int id_1, int id_2){
         if (id_1 > id_2){
             System.out.println(id_1-(id_2+1));
